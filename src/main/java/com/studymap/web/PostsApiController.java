c01a12c862c8266eb9e48880f88546f35cb35bc9
jelly <dear.jojelly@gmail.com>
2021-03-23 17:46:52 +0900

    AipControllerTest 오류 해결 !

--- src/main/java/com/studymap/web/PostsApiController.java
+++ src/main/java/com/studymap/web/PostsApiController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
+
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
         return postsService.save(requestDto);
     }
 
