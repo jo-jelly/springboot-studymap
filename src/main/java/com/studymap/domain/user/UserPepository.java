396a552991586920b2b15ab80b0a61ea0ffa028b
jo-jelly <dear.jojelly@gmail.com>
2021-04-01 02:20:13 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/studymap/domain/user/UserPepository.java
@@ -0,0 +1,10 @@
+package com.studymap.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserPepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
