30db9e16f536e50ffebd3211bf046eb91c0e50bc
jelly <dear.jojelly@gmail.com>
2021-04-02 21:12:56 +0900

    로그인 오류상황

--- src/main/java/com/studymap/web/IndexController.java
+++ src/main/java/com/studymap/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.studymap.web;
 
+import com.studymap.config.auth.dto.SessionUser;
 import com.studymap.service.posts.PostsService;
 import com.studymap.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,23 +9,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
-            model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
-    @GetMapping("posts/save")
-    public String postsSave(){
+    @GetMapping("/posts/save")
+    public String postsSave() {
         return "posts-save";
-        }
-
+    }
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
