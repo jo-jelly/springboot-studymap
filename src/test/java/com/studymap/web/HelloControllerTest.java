64f4ac3835ee6d55af23ced0a962964b8dc1792d
jelly <dear.jojelly@gmail.com>
2021-04-09 06:08:20 +0900

    Test코드 오류수정(가짜 로그인 설정 등)

--- src/test/java/com/studymap/web/HelloControllerTest.java
+++ src/test/java/com/studymap/web/HelloControllerTest.java
@@ -2,6 +2,7 @@ package com.studymap.web;
 
 
 
+import com.studymap.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,13 +22,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @ExtendWith(SpringExtension.class) // junit5
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,  excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+}) //스캔대상에서 SecurityConfig를 제거
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -36,6 +41,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
