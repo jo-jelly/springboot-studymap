dbc2e020d7db59775cfa5fede98614cb0920f8fe
jo-jelly <dear.jojelly@gmail.com>
2021-04-21 05:55:54 +0900

    studyGroupController 화면 나오도록 수정

--- src/main/java/com/studymap/web/studyGroupController.java
+++ src/main/java/com/studymap/web/studyGroupController.java
@@ -1,15 +1,16 @@
 package com.studymap.web;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class studyGroupController {
 
     @GetMapping("/studyGroup")
-    public String hello(){
+    public String studyGroup(){
 
         return "studyGroup";
     }
